580f93f5c3ab5bb539d920d4f15d4b5b01d003ec
90000e <48207536+gamgam330@users.noreply.github.com>
2022-12-23 18:51:43 +0900

    [PYTHON] BOJ
    25304

---
+++ BOJ25304.py
@@ -0,0 +1,12 @@
+total = int(input())
+N = int(input())
+sum = 0
+for i in range(N):
+    a,b = map(int,input().split())
+    sum += a*b
+
+if total == sum:
+    print("Yes")
+else:
+    print("No")
+    
